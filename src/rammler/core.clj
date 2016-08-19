7d7e295f4e8a08ce6d8211b9e63fb3275eaa06b3
Alexander Kahl <alex@lshift.de>
2016-08-19 10:51:11 +0200

    Implicitly validate frames

--- src/rammler/core.clj
+++ src/rammler/core.clj
@@ -78,8 +78,16 @@
 (defcodec reply-text shortstr)
 
 ;;; Protocol
+(defn validate-frame [[type channel payload frame-end]]
+  (println [type channel payload frame-end])
+  (if (= 0xCE (bit-and frame-end 0xFF))
+    [type channel payload]
+    (throw (ex-info "Invalid frame-end byte encountered" {:frame-end frame-end}))))
+
 (defcodec amqp-header [(gloss/string :ascii :length 4) octet octet octet octet])
-(defcodec amqp-frame [octet short (gloss/repeated octet :prefix long) octet])
+(defcodec amqp-frame [octet short (gloss/repeated octet :prefix long) octet]
+  (fn [_] (throw (ex-info "Not implemented" {})))
+  validate-frame)
 (defcodec amqp-method-prefix [short short])
 
 (def payload [1 0 0 0 0 1 -12 0 10 0 10 0 9 0 0 1 -49 12 99 97 112 97 98 105 108 105 116 105 101 115 70 0 0 0 -57 18 112 117 98 108 105 115 104 101 114 95 99 111 110 102 105 114 109 115 116 1 26 101 120 99 104 97 110 103 101 95 101 120 99 104 97 110 103 101 95 98 105 110 100 105 110 103 115 116 1 10 98 97 115 105 99 46 110 97 99 107 116 1 22 99 111 110 115 117 109 101 114 95 99 97 110 99 101 108 95 110 111 116 105 102 121 116 1 18 99 111 110 110 101 99 116 105 111 110 46 98 108 111 99 107 101 100 116 1 19 99 111 110 115 117 109 101 114 95 112 114 105 111 114 105 116 105 101 115 116 1 28 97 117 116 104 101 110 116 105 99 97 116 105 111 110 95 102 97 105 108 117 114 101 95 99 108 111 115 101 116 1 16 112 101 114 95 99 111 110 115 117 109 101 114 95 113 111 115 116 1 15 100 105 114 101 99 116 95 114 101 112 108 121 95 116 111 116 1 12 99 108 117 115 116 101 114 95 110 97 109 101 83 0 0 0 34 114 97 98 98 105 116 64 98 114 111 107 101 114 49 46 97 100 111 114 110 111 46 105 110 46 108 115 104 105 102 116 46 100 101 9 99 111 112 121 114 105 103 104 116 83 0 0 0 46 67 111 112 121 114 105 103 104 116 32 40 67 41 32 50 48 48 55 45 50 48 49 54 32 80 105 118 111 116 97 108 32 83 111 102 116 119 97 114 101 44 32 73 110 99 46 11 105 110 102 111 114 109 97 116 105 111 110 83 0 0 0 53 76 105 99 101 110 115 101 100 32 117 110 100 101 114 32 116 104 101 32 77 80 76 46 32 32 83 101 101 32 104 116 116 112 58 47 47 119 119 119 46 114 97 98 98 105 116 109 113 46 99 111 109 47 8 112 108 97 116 102 111 114 109 83 0 0 0 10 69 114 108 97 110 103 47 79 84 80 7 112 114 111 100 117 99 116 83 0 0 0 8 82 97 98 98 105 116 77 81 7 118 101 114 115 105 111 110 83 0 0 0 5 51 46 54 46 53 0 0 0 14 65 77 81 80 76 65 73 78 32 80 76 65 73 78 0 0 0 5 101 110 95 85 83 -50])
@@ -171,11 +179,6 @@
     :open-ok [[:reserved-1 any]]
     :close [[reply-code reply-code] [reply-text reply-text] [class-id class-id] [method-id method-id]]
     :close-ok []}})
-
-(defn validate-frame [[type channel payload frame-end]]
-  (if (= 0xCE (bit-and frame-end 0xFF))
-    [type channel payload]
-    (throw (ex-info "Invalid frame-end byte encountered" {:frame-end frame-end}))))
 
 (defmulti decode-frame (comp amqp-frame-types first))
 
